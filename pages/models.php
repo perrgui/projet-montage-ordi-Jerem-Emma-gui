e3d0a44c2dd39c8ed7c2ae3d6af7ed3b202fe26e
Wicker-Man <jeremie.malezieux@gmail.com>
2022-02-18 11:50:43 +0100

    login bootstrap

--- pages/models.php
+++ pages/models.php
@@ -2,3 +2,115 @@
 -liste modèle 
 -modifier modèle(archiver, ajouter/retirer, historique( date , nombre d'entrées) )
 -création nouveau modèle -->
+<?php
+
+// nom,quantité (en stock),date ajout, nb (de fois fabriqué), est portable
+
+$modeles = [
+
+    1=> [
+        "nom" => "Dell",
+        "quantité" => 5,
+        "date ajout" => 17/02/2019,
+        "nb" => 15,
+        "est portable" => 1,
+    ],
+
+    2=> [
+        "nom" => "Apple",
+        "quantité" => 20,
+        "date ajout" => 17/02/2022,
+        "nb" => 250,
+        "est portable" => 1,
+    ],
+
+        1=> [
+        "nom" => "Microsoft",
+        "quantité" => 70,
+        "date ajout" => 24/04/2021,
+        "nb" => 186,
+        "est portable" => 1,
+    ],
+
+    3=> [
+        "nom" => "Toshiba",
+        "quantité" => 150,
+        "date ajout" => 03/05/2019,
+        "nb" => 50,
+        "est portable" => 2,
+    ],
+
+        4=> [
+        "nom" => "Sony",
+        "quantité" => 300,
+        "date ajout" => 08/06/2018,
+        "nb" => 100,
+        "est portable" =>  1,
+    ],
+
+    5=> [
+        "nom" => "HP",
+        "quantité" => 200,
+        "date ajout" => 02/01/2022,
+        "nb" => 46,
+        "est portable" =>  2,
+    ],
+
+        6=> [
+        "nom" => "Asus",
+        "quantité" => 483,
+        "date ajout" => 16/11/2021,
+        "nb" => 95,
+        "est portable" => 2,
+    ],
+
+    7=> [
+        "nom" => "Acer",
+        "quantité" => 356,
+        "date ajout" => 04/12/2018,
+        "nb" => 90,
+        "est portable" =>  2,
+    ],
+
+        8=> [
+        "nom" => "Fujitsu",
+        "quantité" => 20,
+        "date ajout" => 14/05/2021,
+        "nb" => 256,
+        "est portable" => 1,
+    ],
+
+    9=> [
+        "nom" => "Lenovo",
+        "quantité" => 263,
+        "date ajout" => 12/06/2021,
+        "nb" => 58,
+        "est portable" => 1,
+    ],
+
+        10=> [
+        "nom" => "LG",
+        "quantité" => 346,
+        "date ajout" => 02/10/2020,
+        "nb" => 632,
+        "est portable" => 1,
+        ],
+
+    ];
+
+    ?>
+    
+        
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modèles</title>
+</head>
+
+
+    
+</body>
+</html>
