59e6fe26ae47ed7919d923ee7ff7b9c65912db03
Cuticule.jeg <92162976+emmasgrl@users.noreply.github.com>
2022-03-03 15:46:47 +0100

    commentaires

--- pages/Monteur/commentaire.php
+++ pages/Monteur/commentaire.php
@@ -1,9 +1,20 @@
 <?php
 // déclaration des variables
-$sql "SELECT * FROM commentaires "
-$req mysql_query ($sql) or die 
-while ($data = mysql_fetch_assoc //Lit une ligne de résultat MySQL dans un tableau associatif
-($req))
+$sql= "SELECT * FROM Commentaires where id= $Commentaires " ;
+$req= mysql_query ($sql) or die 
+while ($data = mysql_fetch_assoc  //Lit une ligne de résultat MySQL dans un tableau associatif
+($req));
+
 {
-    echo 
-}
+    echo "<h1>{$data[" Titre"]}</h1>";
+    echo "<p>{$data["Contenu"]}</p>";
+echo "<p>.date ("j/n/y G:i", strtotime ($data["date"]))."</p>;
+?>
+
+<from action = "index.php" method = "post">
+    idemployé: <input type= "text" name= "idemployé"/></br>
+    contenu:<input type="text" name="date"></br> <textarea name="contenu"></textarea>
+
+
+
+
